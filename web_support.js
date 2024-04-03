e93fb821eaa56bfc80ea01dad1ba10e650eb1c4a
abir8 <abir80503@gmail.com>
2024-04-03 21:00:56 +0600

    final

--- web_support.js
+++
@@ -1,5 +0,0 @@
-<head>
-    <!-- ... -->
-    <script type="application/javascript" src="/assets/packages/flutter_inappwebview_web/assets/web/web_support.js" defer></script>
-    <!-- ... -->
-</head>
